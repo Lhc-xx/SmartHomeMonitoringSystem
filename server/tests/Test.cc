0459f958a6122c701e366e8f206feb2f1641bce9
Ubuntu <ubuntu@localhost.localdomain>
2026-09-03 08:06:38 +0800

    add server/log 相关, resolve double free问题

--- server/tests/Test.cc
+++ server/tests/Test.cc
@@ -1,5 +1,16 @@
 #include "Test.h"
 
+#include "../include/logger.h"
+
 bool Test::run() const {
+    // 首次 getInstance 创建单例并绑定日志文件（写 /tmp，不污染仓库）。
+    Logger::getInstance("/tmp/smart_home_log_test.log");
+
+    LOG_DEBUG("this is a debug message");
+    LOG_INFO("this is an info message");
+    LOG_WARN("this is a warn message");
+    LOG_ERROR("this is an error message");
+
+    Logger::destroy();
     return true;
 }
